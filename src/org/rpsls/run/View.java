d3e4490e3a4307b4f5a7feda0633d14609c76f9f
Aksonenko Oleksii <oaksonenko@gmail.com>
2015-09-08 15:49:52 +0300

    Rebuild code

--- src/org/rpsls/run/View.java
+++ src/org/rpsls/run/View.java
@@ -6,9 +6,10 @@ import org.rspsls.action.Player;
 public class View {
 
 	public static void main (String [] args) {
-		Player pl1 = new Player("Vasya", 3);
-		Player pl2 = new Player("Tolya", 2);
-		Game g1 = new Game(pl1, pl2);
-		ShowWinner.show(g1.findWinner(), pl1.getName(), pl2.getName());
+/*		Player pl1 = new Player("Vasya", 4);
+		Player pl2 = new Player("Tolya", 4);
+		Game2 game = new Game2();
+		game.showWinner(pl1, pl2);*/
+		new Game().showWinner(new Player("Vasya", 2), new Player("Tolya", 3));
 	}
 }
--- src/org/rspsls/action/Game.java
+++ src/org/rspsls/action/Game.java
@@ -2,32 +2,36 @@ package org.rspsls.action;
 
 public class Game {
 	
-	private Player pl1;
+	int winner = 0;
 	
-	private Player pl2;
-	
-	private int winner = 0;
-	
-	public Game(Player pl1, Player pl2) {
-		super();
-		this.pl1 = pl1;
-		this.pl2 = pl2;
-	}
-
-	public int findWinner () {
+	public void findWinner(Player pl1, Player pl2) {
 		int res = (pl1.getChoise() - pl2.getChoise());
-		if(res < 5) {
+		//если результат отрицательное число добавляем 5
+		if(res < 0) {
 			res += 5;
-		}			
+			}
+		//если результат 0 тогда ничья, если больше 0 ищем победителя
 		if (res > 0) {
-			if (res <= 2) {
+			if (res > 2) {
 				winner = 1; // выйграл pl1	
 			} else {
 				winner = 2; // выйграл pl2
 			}
 		}
-		return winner;
+	}
+	
+	public void showWinner(Player pl1, Player pl2) {
+		findWinner(pl1, pl2);
+		switch(winner) {
+		case 0:
+			System.out.println( pl1.getName() + " and " + pl2.getName() + " tie!");
+			break;
+		case 1:
+			System.out.println(pl1.getName() + " wins!");
+			break;
+		case 2:
+			System.out.println(pl2.getName() + " wins!");
+		}
 	}
 
 }
-
